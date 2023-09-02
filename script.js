49ba23f798d8019281aa5e2feea5194a01ab4144
ambatiharshitha <143876921+ambatiharshitha@users.noreply.github.com>
2023-09-02 16:54:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let Celcius= document.getElementById("celcius");
-let Farenheit= document.getElementById("farenheit");
+let Fahrenheit= document.getElementById("fahrenheit");
 
 function celToFar(){
     let output= (parseFloat(Celcius.value)*9/5)+32;
-    Farenheit.value= parseFloat(output.toFixed(2));
+    Fahrenheit.value= parseFloat(output.toFixed(2));
     console.log(output);
 
 } 
 function farToCel(){
-    let output= (parseFloat(Farenheit.value)-32)*5/9;
+    let output= (parseFloat(Fahrenheit.value)-32)*5/9;
     Celcius.value=parseFloat(output.toFixed(2));
     console.log(output);
 } 
